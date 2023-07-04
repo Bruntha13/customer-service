4768d1e3592a644769647895aadc5c377d68b7cd
Bruntha13 <thavabruntha@gmail.com>
2023-07-04 14:36:56 +0530

    Initial commit

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { MyServiceService } from '../my-service.service';
+
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent {
+  constructor(private myservice:MyServiceService) { }
+
+
+  call(){
+      this.myservice.toDo("Button Clicked from test component")
+
+  }
+}
